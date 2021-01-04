95031776bf51771b559bd790f53a35de6c9dc00c
Michelle Jordan <tmsmith1150@live.com>
2021-01-04 16:58:26 -0500

    deploy

--- src/comp/About/index.js
+++ src/comp/About/index.js
@@ -25,7 +25,7 @@ const About = () => {
              <div className="about-title">The Cold Beer Open concept was born in 1980 and organized by Terry Smith and Earl Beahm.</div>
              
              <div className="cbo-about-div">
-             { <img className="cbo-about display-4 brand animate__animated animate__fadeIn animate__delay-1s" src={AboutSlide} alt="about"/> }
+             { <img className="cbo-about display-4 brand animate__animated animate__fadeInDown animate__delay-1s" src={AboutSlide} alt="about"/> }
              </div>
 
              
@@ -46,13 +46,14 @@ const About = () => {
              <div className="about-title title-res">The Cold Beer Open concept was born in 1980 and organized by Terry Smith and Earl Beahm.</div>
              
              <div className="cbo-about-div cbo-res">
-             { <img className="cbo-about display-4 brand animate__animated animate__fadeIn animate__delay-1s" src={AboutSlide} alt="about"/> }
+             { <img className="cbo-about display-4 brand animate__animated animate__fadeInDown animate__delay-1s" src={AboutSlide} alt="about"/> }
              </div>
 
              
           
-             
+             <div className="about-para-div">
              <p className="about-para">The original group consisted of eight golfers taking a trip to Amelia Island, Florida for a weekend of golf and fun. This proved to be one of the best outings anyone could remember. The next year we decided to go to Myrtle Beach and we recruited four more golfers to go with us. That was the year that we adopted the name of The Cold Beer Open. Each year saw more new friends being invited and having a wonderful time. Over the years our group has expanded to where we had 90 golfers in 2009. We have all made new friends and lasting friendships. We have lost some of our participants to bad health and even death. We miss these people greatly and think of them often. The Cold Beer Open concept has always been about friends and having a good time. The Cold Beer Open has never been associated with any business or industry. We do have several sponsors that help us tremendously and we a very grateful to each of them. We wish to acknowledge everyone that has participated over the years but that would just take too much space. Although our name says Open, the tournament is by invitation. A recent participant can invite other people to join our group. We are a very diverse group but this only served to make the friendships even stronger. Again, thanks to everyone that has participated and we look forward to another successful 30 years.</p>
+             </div>
              
              </div>
              </>}
