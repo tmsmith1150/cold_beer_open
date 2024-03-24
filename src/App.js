defddad2a1d6cc429ca3c89ea9d910ca21663d76
Michelle Jordan <tmsmith1150@live.com>
2024-03-24 18:28:55 -0400

    deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import React from 'react';
+import './App.css';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from 'reactstrap';
